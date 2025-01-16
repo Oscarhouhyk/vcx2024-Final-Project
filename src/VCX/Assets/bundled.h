dd8580b53f084347ef2b43fba0f7eb20da590683
oscarhou <yukkit.hou@gmail.com>
2025-01-16 23:25:55 +0800

    remove unwanted

--- src/VCX/Assets/bundled.h
+++ src/VCX/Assets/bundled.h
@@ -21,50 +21,50 @@ namespace VCX::Assets {
     inline constexpr auto ExampleModels {
         std::to_array<std::string_view>({
             "assets/models/arma.obj",
-            "assets/models/block.obj",
+            /*"assets/models/block.obj",
             "assets/models/cube.obj",
             "assets/models/dinosaur.obj",
             "assets/models/face.obj",
             "assets/models/fandisk.obj",
             "assets/models/rocker.obj",
-            "assets/models/sphere.obj",
+            "assets/models/sphere.obj",*/
         })
     };
 
     enum class ExampleModel {
         Arma,
-        Block,
+        /*Block,
         Cube,
         Dinosaur,
         Face,
         Fandisk,
         Rocker,
-        Sphere,
+        Sphere,*/
     };
 
     inline constexpr auto ExampleScenes {
         std::to_array<std::string_view>({
-            "assets/scenes/floor/floor.yaml",
-            "assets/scenes/cornell_box/cornell_box.yaml",
+            //"assets/scenes/floor/floor.yaml",
+            //"assets/scenes/cornell_box/cornell_box.yaml",
             "assets/scenes/teapot/teapot.yaml",
             "assets/scenes/bunny/bunny.yaml",
-            "assets/scenes/sponza/sponza.yaml",
-            "assets/scenes/breakfast_room/breakfast_room.yaml",
+            //"assets/scenes/sponza/sponza.yaml",
+            //"assets/scenes/breakfast_room/breakfast_room.yaml",
             "assets/scenes/white_oak/white_oak.yaml",
             "assets/scenes/sports_car/sports_car.yaml",
-            "assets/scenes/sibenik/sibenik.yaml",
+            //"assets/scenes/sibenik/sibenik.yaml",
         })
     };
 
     enum class ExampleScene {
-        Floor,
-        CornellBox,
+        //Floor,
+        //CornellBox,
         Teapot,
         Bunny,
-        Sponza,
-        BreakfastRoom,
+        //Sponza,
+        //BreakfastRoom,
         WhiteOak,
         SportsCar,
-        Sibenik,
+        //Sibenik,
     };
 }
